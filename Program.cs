0d67d5870c00de92c08a042f545dee592418957f
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-05-09 23:01:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ class Program
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
+    public static AppBuilder BuildAvaloniaApp() 
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
